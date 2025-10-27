8edf24290398f6c8bd16518f83aea453186498a5
jihan-kim99 <jinnkenny99@gmail.com>
2025-10-26 21:28:00 -0300

    ops

---
+++ infra/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-blazor-prod"
+  location = "eastus"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "asp-blazor-prod"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku_name            = "P1v3"
+  os_type             = "Linux"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "blazor-webapp-prod"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    always_on            = true
+    ftps_state           = "Disabled"
+    application_stack {
+      dotnet_version = "8.0"
+    }
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+}
